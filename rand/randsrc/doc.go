acc3d383548cd3d01ad3b28c543d49cedd48b3f6
Leandro Motta Barros <lmb@stackedboxes.org>
2016-09-19 20:23:30 -0300

    Making the package a bit more findable.

--- rand/randsrc/doc.go
+++ rand/randsrc/doc.go
@@ -1,4 +1,5 @@
-// Package randsrc provides some random number sources.
+// Package randsrc provides random number sources based on algorithms like
+// Xoroshiro128+ and SplitMix64.
 //
 // Things here should be good enough for games and creative coding. Don't use
 // them in applications that depend critically on random numbers (criptography,
