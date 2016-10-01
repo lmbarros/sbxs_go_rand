c774bf701ec4286d34bb6f4bff25757cf47cf84c
Leandro Motta Barros <lmb@stackedboxes.org>
2016-10-01 16:36:43 -0300

    Added example to check the RNGs state sizes.

---
+++ randsrc/sizeof_example_test.go
@@ -0,0 +1,21 @@
+package randsrc
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+// Not a real example, but rather a simple tool to check each algorithm's state
+// size.
+func Example_sizeof() {
+	fmt.Printf("KnuthLCG: %v\n", unsafe.Sizeof(knuthLCGSource{}))
+	fmt.Printf("MT19937-64: %v\n", unsafe.Sizeof(mt19937_64Source{}))
+	fmt.Printf("SplitMix64: %v\n", unsafe.Sizeof(splitMix64Source{}))
+	fmt.Printf("Xoroshiro128+: %v\n", unsafe.Sizeof(xoroshiro128PlusSource{}))
+
+	// Output:
+	// KnuthLCG: 8
+	// MT19937-64: 2504
+	// SplitMix64: 8
+	// Xoroshiro128+: 16
+}
